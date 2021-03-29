383e043926f2e3315f2f5c78553d4382c312416a
MartykDev <vitalii.martyniuk.common@gmail.com>
2021-03-29 18:42:54 +0300

    Added base classes

---
+++ HES.Web/Components/HESPageBase.cs
@@ -0,0 +1,49 @@
+﻿using HES.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace HES.Web.Components
+{
+    public class HESPageBase : HESBase
+    {
+        [Inject] protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        [Inject] protected ISynchronizationService SynchronizationService { get; set; }
+        [Inject] protected IModalDialogService2 ModalDialogService2 { get; set; }
+        [Inject] protected IBreadcrumbsService BreadcrumbsService { get; set; }
+
+        protected string PageId { get; private set; }
+        protected bool LoadFailed { get; private set; }
+        protected string ErrorMessage { get; private set; }
+
+        protected async Task<string> GetCurrentUserEmailAsync()
+        {
+            return (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User.Identity.Name;
+        }
+
+        protected override void SetInitialized()
+        {
+            PageId = Guid.NewGuid().ToString();
+            base.SetInitialized();
+        }
+
+        protected void SetLoadFailed(string message)
+        {
+            ErrorMessage = message;
+            LoadFailed = true;
+        }
+
+        protected void SetErrorMessage(string message)
+        {
+            ErrorMessage = message;
+            StateHasChanged();
+        }
+
+        protected void ClearErrorMessage()
+        {
+            ErrorMessage = string.Empty;
+            StateHasChanged();
+        }
+    }
+}
