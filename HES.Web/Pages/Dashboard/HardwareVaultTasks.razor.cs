b562d5e685d49eb127d43f2925b858745547d5bd
CORE <namespacedevbox@gmail.com>
2021-03-30 13:26:26 +0300

    Updated dashboard modals

--- HES.Web/Pages/Dashboard/HardwareVaultTasks.razor.cs
+++ HES.Web/Pages/Dashboard/HardwareVaultTasks.razor.cs
@@ -1,21 +1,33 @@
 ﻿using HES.Core.Entities;
+using HES.Core.Enums;
 using HES.Core.Interfaces;
+using HES.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace HES.Web.Pages.Dashboard
 {
-    public partial class HardwareVaultTasks : ComponentBase
+    public partial class HardwareVaultTasks : HESModalBase
     {
         [Inject] public IHardwareVaultTaskService HardwareVaultTaskService { get; set; }
-        [Inject] public IModalDialogService ModalDialogService { get; set; }
-
+        [Inject] public ILogger<HardwareVaultTasks> Logger { get; set; }
         public List<HardwareVaultTask> HardwareVaultTaskList { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            HardwareVaultTaskList = await HardwareVaultTaskService.GetHardwareVaultTasksAsync();
+            try
+            {
+                HardwareVaultTaskList = await HardwareVaultTaskService.GetHardwareVaultTasksAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                await ToastService.ShowToastAsync(ex.Message, ToastType.Error);
+                await ModalDialogCancel();
+            }
         }
     }
 }
