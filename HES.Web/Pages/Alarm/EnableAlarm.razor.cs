b67096070aff1d4b5ab117ff86fd3187fdff8ca8
CORE <namespacedevbox@gmail.com>
2021-03-30 13:00:32 +0300

    Updated alarm modals

--- HES.Web/Pages/Alarm/EnableAlarm.razor.cs
+++ HES.Web/Pages/Alarm/EnableAlarm.razor.cs
@@ -9,41 +9,30 @@ using System.Threading.Tasks;
 
 namespace HES.Web.Pages.Alarm
 {
-    public partial class EnableAlarm : HESComponentBase
+    public partial class EnableAlarm : HESModalBase
     {
         public IRemoteWorkstationConnectionsService RemoteWorkstationConnections { get; set; }
-        [Inject] public IToastService ToastService { get; set; }
         [Inject] public ILogger<EnableAlarm> Logger { get; set; }
-        [CascadingParameter] public ModalDialogInstance ModalDialogInstance { get; set; }
+        [Parameter] public string CurrentUserEmail { get; set; }
+
+        protected override void OnInitialized()
+        {
+            RemoteWorkstationConnections = ScopedServices.GetRequiredService<IRemoteWorkstationConnectionsService>();
+        }
 
         private async Task EnableAlarmAsync()
         {
             try
             {
-                RemoteWorkstationConnections = ScopedServices.GetRequiredService<IRemoteWorkstationConnectionsService>();
-
-                string userEmail = null;
-                try
-                {
-                    userEmail = (await AuthenticationStateTask).User.Identity.Name;
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogError(ex.Message);
-                }
-                finally
-                {
-                    await RemoteWorkstationConnections.LockAllWorkstationsAsync(userEmail);
-                }
-
+                await RemoteWorkstationConnections.LockAllWorkstationsAsync(CurrentUserEmail);
                 await ToastService.ShowToastAsync("All workstations are locked.", ToastType.Success);
-                await ModalDialogInstance.CloseAsync(ModalResult.Success);
+                await ModalDialogClose();
             }
             catch (Exception ex)
             {
                 Logger.LogError(ex.Message, ex);
                 await ToastService.ShowToastAsync(ex.Message, ToastType.Error);
-                await ModalDialogInstance.CloseAsync(ModalResult.Cancel);
+                await ModalDialogCancel();
             }
         }
     }
